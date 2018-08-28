e46d16d61e2fe1cfb28d2aa3447c3ef4352953a5
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:34:10 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -25,8 +25,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "SELECT authors.name,series.subgenres
+  "SELECT authors.name, series.subgenres
   FROM authors
+  JOIN series
+  on 
   ;"
 end
 
