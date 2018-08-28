fa0838537417c1d1977ed91d07312449d7510169
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:37:40 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -30,7 +30,7 @@ def select_name_and_series_subgenres_of_authors
   JOIN series
   ON authors.id=series.author_id
   JOIN subgenres
-  ON subgenres.author_id=series._author_id
+  ON subgenres.id=series.subgenre_id
   ;"
 end
 
