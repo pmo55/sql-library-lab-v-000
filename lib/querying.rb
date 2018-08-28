b547dffd9db11cfd1aa5f67b6c85a40c9c0cd124
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:53:47 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -50,5 +50,6 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
- "SELECT characters.names, COUNT(characters.name);"
+ "SELECT characters.names, COUNT
+ FROM characters;"
 end
