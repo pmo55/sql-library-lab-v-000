888c5a39725fd0129a6cedc4d0c11bb3da731bf3
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:41:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -36,7 +36,10 @@ end
 
 def select_series_title_with_most_human_characters
   "SELECT series.title
-  FROM series;"
+  FROM series
+  JOIN characters
+  ON 
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in
