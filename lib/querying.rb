b209c86d541c6880b2b0c15b1234069737263ffc
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:56:30 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -53,5 +53,7 @@ def select_character_names_and_number_of_books_they_are_in
  "SELECT characters.names, COUNT(*) as book_count
  FROM character_books
  JOIN characters
- ON characters.id=character_books.character_id;"
+ ON characters.id=character_books.character_id
+ GROUP BY charcters.name
+ ;"
 end
