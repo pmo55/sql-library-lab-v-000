996c37d4f99611fb0e696d8d3c8c2096ade7cdc8
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:24:24 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,12 +8,11 @@ def select_books_titles_and_years_in_first_series_order_by_year
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "SELECT name, characters.motto
-  FROM characters
+  "SELECT authors.name, characters.motto
+  FROM authors
   JOIN characters
   ON authors.id=characters.author_id
-  ORDER BY Length(motto) DESC;
-  "
+  ORDER BY Length(motto) DESC;"
 end
 
 
