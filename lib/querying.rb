83b09ea542e8d66e43955bbe484f6004422d39e1
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 00:41:44 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -11,7 +11,6 @@ def select_name_and_motto_of_char_with_longest_motto
   "SELECT authors.name, characters.motto
   FROM authors
   JOIN characters
-  ON authors.id=characters.author_id
   ORDER BY Length(motto) DESC;
   
   "
