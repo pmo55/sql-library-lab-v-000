3ef4ca360639465e9e3fb503305ae84442d0079c
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:50:04 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,7 +42,7 @@ def select_series_title_with_most_human_characters
   JOIN characters
   ON characters.series_id=series.id
   JOIN character_books
-  ON character_books.books_id=books.id
+  ON character_books.book_id=books.id
   WHERE characters.species='humans'
   GROUP BY series.title;
   "
