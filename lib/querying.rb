419044510ec1a6d2a678d8683846572eb1112492
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 01:42:48 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,7 +39,7 @@ def select_series_title_with_most_human_characters
   FROM series
   JOIN characters
   ON characters.series_id=series.id
-  LIMIT 1;
+  WHERE characters.species=humans;
   "
 end
 
