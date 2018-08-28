55e8e67aefa4a812e6446159843864499fcd34ff
Parsa Moshiri <parsamoshiri5@gmail.com>
2018-08-28 00:22:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -3,7 +3,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   FROM books
   JOIN series
   ON books.series_id=series.id
-  WHERE series.id=1
+  WHERE books.series_id=1
   ORDER BY year;"
 end
 
